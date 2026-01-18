e249daa756a01d5b95134fd11e06a52de8c0a8e1
Ganesh Malo <malo.ganesh98@gmail.com>
2026-01-18 22:11:11 +0530

    Binary search tree question added

--- arrayToBST.js
+++ arrayToBST.js
@@ -1,3 +1,8 @@
+/*Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+Example 1:
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:*/
 
 function sortedArrayToBST(nums) {
   function createTree(left, right) {
